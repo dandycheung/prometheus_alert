ad4315bc744005681542cc1e561f2b315ccf3f56
git <smxblysq@qq.com>
2022-10-31 18:10:11 +0800

    新增代码

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from http import HTTPStatus
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode, quote_plus
 from urllib.error import HTTPError
-from settings import NOTICE_SETTINGS
+from settings import NOTICE_SETTINGS, HOST
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 try:
@@ -394,7 +394,7 @@ def webhook():
             prometheus_monitor_info=prometheus_data
         )
         filename = os.path.join('templates', "{}.html".format(int(time.time())))
-        full_url = os.path.join("http://10.200.34.5:5000", filename)
+        full_url = os.path.join(HOST, filename)
         write_html_file(filename=filename, content=html_template_content)
         # 获取收件人邮件列表
         email_list = get_email_conf('email.yaml', email_name=team_name, action=0)
--- settings.tmp.py
+++ settings.tmp.py
@@ -1,1 +1,2 @@
 NOTICE_SETTINGS = [{'token': 'aa04f265-b8a7-40e3-8e46-90a73fdce9c1', 'msg_type': 'WECHAT_ROBOT'}]
+HOST = "http://10.200.34.5:5000"
