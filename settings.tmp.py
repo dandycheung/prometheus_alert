af28e21230c90f9e1fb9eeebd52eeb34e822b128
git <smxblysq@qq.com>
2022-11-01 09:16:52 +0800

    新增代码

--- settings.tmp.py
+++ settings.tmp.py
@@ -1,2 +1,2 @@
-NOTICE_SETTINGS = [{'token': 'aa04f265-b8a7-40e3-8e46-90a73fdce9c1', 'msg_type': 'WECHAT_ROBOT'}]
-HOST = "http://10.200.34.5:5000"
+NOTICE_SETTINGS = [{'token': '', 'msg_type': 'WECHAT_ROBOT'}]
+HOST = "http://xx.xxxx.xx.xx:5000"
